e173c3b65c83fc02002078ec01d3d027279ac822
Gagana <120469090+GaganaNarasimaiah@users.noreply.github.com>
2023-03-06 06:39:30 -0800

    Add files via upload

---
+++ Netflix.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+const Netflix=()=>{
+    return(
+        <>
+        <div className='mainblock'>
+        <nav className='navblock'>
+         <div>
+           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVAL0ko_HJffcALsQvKq5RG1GZGc7UpZs7NRZ-ITOOZPzMLVTiLaQ5hzRUOz1nbDo3UMI&usqp=CAU" alt="" />
+         </div>
+        <div className='contentblock'>
+        <ul>
+         <li><h3><a href="">search</a></h3></li>
+        </ul>
+        <ul>
+         <li><h3><a href="">catogories</a></h3></li>
+        </ul>
+        <ul>
+         <li><h3><a href="">Movies</a></h3></li>
+        </ul>
+        <ul>
+         <li><h3><a href="">TVshowes</a></h3></li>
+        </ul>
+     </div>
+     </nav>
+     </div>
+     </>
+     
+      )
+     }
+     
+     export default Netflix;
+     
