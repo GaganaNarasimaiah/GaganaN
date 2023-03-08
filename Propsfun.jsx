a46cf7a69f8d02a68d5306d9df2d3abb879f89dd
Gagana <120469090+GaganaNarasimaiah@users.noreply.github.com>
2023-03-08 07:14:51 -0800

    Add files via upload

---
+++ Propsfun.jsx
@@ -0,0 +1,24 @@
+// import React from "react"
+// const Propsfun=(props)=>{
+//     return(
+// <>
+//     <h1>{props.data}</h1>
+// </>
+//     )
+// }
+
+
+import React from "react"
+import { Component } from "react";
+class Propsfun extends Component {
+  render() {
+    return (<>
+      {/* <div>{this.props.data}</div>
+      <h1>{this.props.data1}</h1>
+    <img src={this.props.data2} alt="" /> */}
+          {/* <img src={this.props.data} alt="" />  */}
+      </>
+    )
+  }
+}
+export default Propsfun;
