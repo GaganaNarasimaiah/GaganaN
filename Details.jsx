fbe70aa978e5d25ac7112eeebf7d16d79c38dcee
Gagana <120469090+GaganaNarasimaiah@users.noreply.github.com>
2023-03-09 09:23:56 -0800

    Add files via upload

---
+++ Details.jsx
@@ -0,0 +1,160 @@
+import React from 'react'
+
+const Details=(props)=>{
+    console.log(props);
+    return(
+        <>
+        <div className='parentblock'>
+            <div>
+            <div className='childblock'>
+            <img src={props.payload[0].photo} alt="" />
+            <h2>{props.payload[0].movieName}</h2>
+            <h3>{props.payload[0].YORelease}</h3>
+            <h3>{props.payload[0].Cast}</h3>
+            <h3>{props.payload[0].rating}</h3>
+            </div>
+            <div className='childblock'>
+            <img src={props.payload[1].photo} alt="" />
+            <h2>{props.payload[1].movieName}</h2>
+            <h3>{props.payload[1].YORelease}</h3>
+            <h3>{props.payload[1].Cast}</h3>
+            <h3>{props.payload[1].rating}</h3>
+            </div>
+            <div className='childblock'>
+            <img src={props.payload[2].photo} alt="" />
+            <h2>{props.payload[2].movieName}</h2>
+            <h3>{props.payload[2].YORelease}</h3>
+            <h3>{props.payload[2].Cast}</h3>
+            <h3>{props.payload[2].rating}</h3>
+            </div>
+            <div className='childblock'>
+            <img src={props.payload[3].photo} alt="" />
+            <h2>{props.payload[3].movieName}</h2>
+            <h3>{props.payload[3].YORelease}</h3>
+            <h3>{props.payload[3].Cast}</h3>
+            <h3>{props.payload[3].rating}</h3>
+            </div>
+            <div className='childblock'>
+            <img src={props.payload[4].photo} alt="" />
+            <h2>{props.payload[4].movieName}</h2>
+            <h3>{props.payload[4].YORelease}</h3>
+            <h3>{props.payload[4].Cast}</h3>
+            <h3>{props.payload[4].rating}</h3>
+            </div>
+            </div>
+            <div>
+            <div className='childblock'>
+            <img src={props.payload[5].photo} alt="" />
+            <h2>{props.payload[5].movieName}</h2>
+            <h3>{props.payload[5].YORelease}</h3>
+            <h3>{props.payload[5].Cast}</h3>
+            <h3>{props.payload[5].rating}</h3>
+            </div>
+            <div className='childblock'>
+            <img src={props.payload[6].photo} alt="" />
+            <h2>{props.payload[6].movieName}</h2>
+            <h3>{props.payload[6].YORelease}</h3>
+            <h3>{props.payload[6].Cast}</h3>
+            <h3>{props.payload[6].rating}</h3>
+            </div>
+            <div className='childblock'>
+            <img src={props.payload[7].photo} alt="" />
+            <h2>{props.payload[7].movieName}</h2>
+            <h3>{props.payload[7].YORelease}</h3>
+            <h3>{props.payload[7].Cast}</h3>
+            <h3>{props.payload[7].rating}</h3>
+            </div>
+            <div className='childblock'>
+            <img src={props.payload[8].photo} alt="" />
+            <h2>{props.payload[8].movieName}</h2>
+            <h3>{props.payload[8].YORelease}</h3>
+            <h3>{props.payload[8].Cast}</h3>
+            <h3>{props.payload[8].rating}</h3>
+            </div>
+            <div className='childblock'>
+            <img src={props.payload[9].photo} alt="" />
+            <h2>{props.payload[9].movieName}</h2>
+            <h3>{props.payload[9].YORelease}</h3>
+            <h3>{props.payload[9].Cast}</h3>
+            <h3>{props.payload[9].rating}</h3>
+            </div>
+            </div>
+            <div>
+            <div className='childblock'>
+            <img src={props.payload[10].photo} alt="" />
+            <h2>{props.payload[10].movieName}</h2>
+            <h3>{props.payload[10].YORelease}</h3>
+            <h3>{props.payload[10].Cast}</h3>
+            <h3>{props.payload[10].rating}</h3>
+            </div>
+            <div className='childblock'>
+            <img src={props.payload[11].photo} alt="" />
+            <h2>{props.payload[11].movieName}</h2>
+            <h3>{props.payload[11].YORelease}</h3>
+            <h3>{props.payload[11].Cast}</h3>
+            <h3>{props.payload[11].rating}</h3>
+            </div>
+            <div className='childblock'>
+            <img src={props.payload[12].photo} alt="" />
+            <h2>{props.payload[12].movieName}</h2>
+            <h3>{props.payload[12].YORelease}</h3>
+            <h3>{props.payload[12].Cast}</h3>
+            <h3>{props.payload[12].rating}</h3>
+            </div>
+            <div className='childblock'>
+            <img src={props.payload[13].photo} alt="" />
+            <h2>{props.payload[13].movieName}</h2>
+            <h3>{props.payload[13].YORelease}</h3>
+            <h3>{props.payload[13].Cast}</h3>
+            <h3>{props.payload[13].rating}</h3>
+            </div>
+            <div className='childblock'>
+            <img src={props.payload[14].photo} alt="" />
+            <h2>{props.payload[14].movieName}</h2>
+            <h3>{props.payload[14].YORelease}</h3>
+            <h3>{props.payload[14].Cast}</h3>
+            <h3>{props.payload[14].rating}</h3>
+            </div>
+            </div>
+            <div>
+            <div className='childblock'>
+            <img src={props.payload[15].photo} alt="" />
+            <h2>{props.payload[15].movieName}</h2>
+            <h3>{props.payload[15].YORelease}</h3>
+            <h3>{props.payload[15].Cast}</h3>
+            <h3>{props.payload[15].rating}</h3>
+            </div>
+            <div className='childblock'>
+            <img src={props.payload[16].photo} alt="" />
+            <h2>{props.payload[16].movieName}</h2>
+            <h3>{props.payload[16].YORelease}</h3>
+            <h3>{props.payload[16].Cast}</h3>
+            <h3>{props.payload[16].rating}</h3>
+            </div>
+            <div className='childblock'>
+            <img src={props.payload[17].photo} alt="" />
+            <h2>{props.payload[17].movieName}</h2>
+            <h3>{props.payload[17].YORelease}</h3>
+            <h3>{props.payload[17].Cast}</h3>
+            <h3>{props.payload[17].rating}</h3>
+            </div>
+            <div className='childblock'>
+            <img src={props.payload[18].photo} alt="" />
+            <h2>{props.payload[18].movieName}</h2>
+            <h3>{props.payload[18].YORelease}</h3>
+            <h3>{props.payload[18].Cast}</h3>
+            <h3>{props.payload[18].rating}</h3>
+            </div>
+            <div className='childblock'>
+            <img src={props.payload[19].photo} alt="" />
+            <h2>{props.payload[19].movieName}</h2>
+            <h3>{props.payload[19].YORelease}</h3>
+            <h3>{props.payload[19].Cast}</h3>
+            <h3>{props.payload[19].rating}</h3>
+            </div>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Details;
