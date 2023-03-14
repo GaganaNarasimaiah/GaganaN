f607cda4aa617e8726b0d3472ee2632c602f27e2
Gagana <120469090+GaganaNarasimaiah@users.noreply.github.com>
2023-03-14 07:32:02 -0700

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,42 +1,15 @@
-import React from 'react'
-// import Nav from './component/Nav'
-// import './global.css'
-// import Navbars from './Navbars';
-// import './navbars.css'
-// import Netflix from './Netflix';
-// import './netflix.css'
-// import Propsfun from './Propsfun';
-// import photo from './picture.jpg';
-// import JSON from './moviesdata.json';
-// import Details from './Details';
-// import './details.css'
-import Courses from './Courses'
+import React, { Component } from 'react'
+import JSON from './std.json'
+import Studetails from './Studetails'
 
-
-const App =()=> {
+export default class App extends Component {
+    constructor(){
+        super()
+        this.state={json:JSON}
+    }
+  render() {
     return (
-        <>
-        {/* <Nav/>
-       <Navbars/>
-      <Netflix/>  */}
-     {/* <Propsfun data="priya"/>
-       <Propsfun data1="priya"/>
-       <Propsfun data2={photo}/>
-       <Propsfun data={"https://nationaltoday.com/wp-content/uploads/2021/08/Teddy-Bear-Day.jpg"}/>  */}
-       {/* <Propsfun data5={{
-        name:"gagana", password:"1234"
-       }}/> */}
-       {/* <Details payload={JSON} /> */}
-       {/* <Courses coursedata={{
-        courseName:"java full stack",
-        courseDuration:"3months",
-        courseAvail:true
-       }}/> */}
-       
-
-
-       <Courses/>
-       </>
+      <Studetails data={this.state.json} />
     )
+  }
 }
-export default App;
