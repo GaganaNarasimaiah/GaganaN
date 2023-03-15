4d01e8e5d664066a9a6dddf2c2c86f4e69b1c4fa
Gagana <120469090+GaganaNarasimaiah@users.noreply.github.com>
2023-03-15 06:59:28 -0700

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,15 +1,47 @@
+// import React, { Component } from 'react'
+// import JSON from './std.json'
+// import Studetails from './Studetails'
+
+// export default class App extends Component {
+//     constructor(){
+//         super()
+//         this.state={json:JSON}
+//     }
+//   render() {
+//     return (
+//       <Studetails data={this.state.json} />
+//     )
+//   }
+// }
+
 import React, { Component } from 'react'
-import JSON from './std.json'
-import Studetails from './Studetails'
+import  './Counter.css'
 
 export default class App extends Component {
-    constructor(){
-        super()
-        this.state={json:JSON}
-    }
+
+  constructor(){
+    super()
+   this.state={count:0}
+   }
+handleIncremnt=()=>{
+  this.setState({count:this.state.count+1})
+}
+handleDecremnt=()=>{
+  this.setState({count:this.state.count-1})
+}
+handleReset=()=>{
+  this.setState({count:0})
+}
   render() {
     return (
-      <Studetails data={this.state.json} />
+      <div className='maindiv'>
+        <h1><i>{this.state.count}</i></h1>
+        <div className='buttondiv'>
+        <button onClick={this.handleIncremnt}>INCREMENT</button>
+        <button onClick={this.handleDecremnt}>DECREMENT</button>
+        <button onClick={this.handleReset}>RESET</button>
+        </div>
+        </div>
     )
   }
 }
